57a4a3bfdf4c16d625f340b8fa154d2d05958c8e
amieow <109803246+amieow@users.noreply.github.com>
2023-08-23 22:38:06 +0700

    fix answer

--- answer advanced.js
+++ answer advanced.js
@@ -1,9 +1,12 @@
 function reservedNumber(N){
+    let result = ""
     for(let i = N ;i>0;i--){
         let str = ""
         for(let y = i;y>0;y--){
             str += y + " "
         }
-        console.log(str)
+        result += str + "\n"
     }
+    return result
 }
+console.log(reservedNumber(5))
